cbdb9a83f20a97131b36c716aea5216e33cdeecf
888-hchecker <hchecker7@gmail.com>
2025-07-13 10:35:02 +0000

    conver

---
+++ basic/test4.js
@@ -0,0 +1,16 @@
+let str1="1"
+let str2=3
+console.log(str1 + str2)
+let str3=Number(str1)
+console.log(str3 + str2)
+console.log(1 + 2+ "3")
+console.log("3"+1+2)
+console.log(str2 + str3)
+console.log(str3 - str2)
+console.log(str2*str3)
+console.log(str2**str3)
+console.log(str2%str3)
+console.log(str2/str3)
+let str4=-(str2)
+console.log(str4)
+
