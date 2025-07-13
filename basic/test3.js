1d8d242c4eeaea87d7912dba99542e80275ace86
888-hchecker <hchecker7@gmail.com>
2025-07-13 10:05:48 +0000

    convertion

---
+++ basic/test3.js
@@ -0,0 +1,21 @@
+let number =33
+console.log(number)
+console.log(typeof number)
+console.log(typeof(number))
+let num1="33abc"
+console.log(typeof num1)
+let num2=Number(num1)
+console.log(num2)
+let num3=null
+console.log(num3)
+let num4= undefined
+console.log(num4)
+//33=> easily converted string to number
+//33abc=> NaN
+let num5=23
+let num6=String(num5)
+console.log(typeof num6)
+let num7=1
+let num8=Boolean(num7)
+console.log(typeof num8)
+console.log(num8)
